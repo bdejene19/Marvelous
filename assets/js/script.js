9179f00444846dc67461ad7938cd55c87cc32dda
Bemnet Dejene <bdejene19@gmail.com>
2022-02-01 20:53:04 -0500

    Added api integration of tmdb popular movies to the home page. Finished function for generating movie cards, also finished function that integrates generated cards into UI

--- assets/js/script.js
+++ assets/js/script.js
@@ -23,31 +23,82 @@ async function getPopularFromTmdbApi() {
  * 
  */
 const generatePopularMovies = async () => {
+    // me getting popular data from tmdb api
    let myPopular = await getPopularFromTmdbApi();
-   let cardContainer = document.createElement('article');
+
+    // creating variables for future use
+   let backdrop = '';
+   let movieName = '';
+   let movieDescription = '';
+   let releaseDate = '';
+   let voteScore = 0;
+
+   /**
+    * needed attributes:
+    * .backdrop_path OR .poster_path
+    * realse_date
+    * original_title
+    * vote_average
+    * overview
+    */
+    // iterate through api array response 
+   myPopular.forEach(movie => {
+        // set variables to pass into function, to generate html card elements
+       backdrop = movie.poster_path;
+       movieName = movie.original_title;
+       releaseDate = movie.release_date;
+       movieDescription = movie.overview;
+       voteScore = movie.vote_average;
+
+       createMovieCard(movieName, releaseDate, backdrop, voteScore, movieDescription);
+   })
+   return myPopular;
 
 //    let movieName = '';
 //    let releaseDate = '';
-//    let coverURL = '';   
-    cardContainer.setAttribute('class', 'popular-movie');
-
+//    let coverURL = ''; 
 }
 
-const createMovieCard = (name, releaseDate, coverPhoto) => {
+const createMovieCard = (name, releaseDate, coverPhoto, vote_score, overview) => {
     let cardContainer = document.createElement('article');
     cardContainer.setAttribute('class', 'display-card');
 
+
+    // movie poster photo
     let coverImg = document.createElement('img');
-    coverImg.setAttribute('src', coverPhoto);
+    coverImg.setAttribute('src', `https://image.tmdb.org/t/p/w500/${coverPhoto}` );
     coverImg.setAttribute('alt', name);
 
+    let vote_score_container = document.createElement('span');
+    let textContainer = document.createElement('div');
 
-    cardContainer.append(coverImg, name, releaseDate);
 
-    document.querySelector('#search-results').append(cardContainer);
+    vote_score_container.textContent = vote_score;
+    let textColor = '';
+
+    if (vote_score >= 8) {
+        textColor = 'green';
+    } else if (vote_score >= 7 && vote_score < 8) {
+        textColor = 'orange';
+    } else {
+        textColor = 'red';
+    }
+    vote_score_container.style.color = textColor;
+
+    let movieTitle = document.createElement('h3');
+    movieTitle.textContent = name;
+
+
+    let movieRelease = document.createElement('p');
+    movieRelease.textContent = releaseDate;
+
+    textContainer.append(movieTitle, movieRelease);
+    cardContainer.append(coverImg, vote_score_container, textContainer);
+
+    document.querySelector('#popular').append(cardContainer);
 }
 
-createMovieCard('Bemnet', '03/2/2020', 'https://pbs.twimg.com/media/D8Dp0c5WkAAkvME?format=jpg&name=900x900')
+// createMovieCard('Bemnet', '03/2/2020', 'https://pbs.twimg.com/media/D8Dp0c5WkAAkvME?format=jpg&name=900x900')
 
 /**
  * 
@@ -64,7 +115,7 @@ function getVideoTrailerById(itemId) {
 
 
 async function test() {
-    let res = await getPopularFromTmdbApi();
+    let res =  await generatePopularMovies();
     console.log(res);
 }
 
